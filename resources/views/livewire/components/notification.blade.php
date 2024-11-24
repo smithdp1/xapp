7210cab8b6227a71222a10cca41a705e0e84e292
David Smith <=>
2024-11-24 12:32:21 -0700

    b

--- resources/views/livewire/components/notification.blade.php
+++ resources/views/livewire/components/notification.blade.php
@@ -2,8 +2,8 @@
     <div class="mt-2 flex-shrink-0 text-blue-400">
         {!! $this->getIcon() !!}
     </div>
-    {{dd($this->notification->data)}} 
-    {{-- <div class="flex-1">
+    {{-- {{dd($this->notification->data)}}  --}}
+    <div class="flex-1">
         <a href="{{ route('profile.show', $this->getUser()) }}" wire:navigate class="flex items-center space-x-3">
             <img src="{{ $this->getUser()->profile_photo_url }}" alt="User Image" class="w-10 h-10 rounded-full">
         </a>
@@ -37,5 +37,5 @@
                 <p><a href="{{ route('profile.show', $this->getUser()) }}" wire:navigate class=" hover:underline font-bold">{{ $this->getUser()->name }}</a> followed you.</p>
             </div>
         @endif
-    </div> --}}
+    </div>
 </div>
