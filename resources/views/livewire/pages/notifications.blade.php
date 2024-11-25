4f42a1c31670c45c6955407ed7a79444b549071d
David Smith <=>
2024-11-25 09:08:58 -0700

    remove div

--- resources/views/livewire/pages/notifications.blade.php
+++ resources/views/livewire/pages/notifications.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full sm:w-600 h-screen">
+
     <div class="w-full sm:w-600 h-screen">
         <div
             class="flex justify-between items-center border-b px-4 py-3 sticky top-0 bg-white dark:bg-dim-900 border-l border-r border-gray-200 dark:border-gray-700"
@@ -14,4 +14,4 @@
             @endforeach
         </div>
     </div>
-</div>
+
