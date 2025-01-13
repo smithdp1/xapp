f630d0b50c5fa861e8e3f1e250a591422dde39d8
David Smith <=>
2025-01-13 11:53:10 -0700

    photo fix

--- resources/views/livewire/components/profile/edit-profile.blade.php
+++ resources/views/livewire/components/profile/edit-profile.blade.php
@@ -17,7 +17,7 @@
             <h2 class="mb-0 text-xl font-bold text-gray-900 dark:text-white">
                 {{ $user->username }}
                 <p class="mb-0 w-48 text-xs text-gray-900 dark:text-gray-400">
-                    {{ $user->tweets->count() }} Tweets
+                    {{ $user->tweets->count() }} Posts
                 </p>
             </h2>
         </div>
@@ -27,7 +27,7 @@
         <div class="w-full bg-cover bg-no-repeat bg-center"
              style="height: 200px; background-image: url('/{{ $user->banner_path ?? 'https://via.placeholder.com/600x200' }}');">
             <img class="opacity-0 w-full h-full"
-                 src="url/{{ $user->banner_path ?? 'https://via.placeholder.com/600x200' }}" alt="">
+                 src="{{ $user->banner_path ?? 'https://via.placeholder.com/600x200' }}" alt="">
         </div>
         <div class="p-4">
             <div class="relative flex w-full">
