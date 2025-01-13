500ac5fbecc042969ca0a380d2e4507a1d210f65
David Smith <=>
2025-01-13 13:37:17 -0700

    photo

--- resources/views/livewire/components/profile/edit-profile.blade.php
+++ resources/views/livewire/components/profile/edit-profile.blade.php
@@ -25,7 +25,7 @@
     <!-- User card-->
     <div>
         <div class="w-full bg-cover bg-no-repeat bg-center"
-             style="height: 200px; background-image: url('{{ $user->banner_path ?? 'https://via.placeholder.com/600x200' }}');">
+             style="height: 200px; background-image: url('{{ asset($user->banner_path) ?? 'https://via.placeholder.com/600x200' }}');">
             <img class="opacity-0 w-full h-full"
                  src="{{ $user->banner_path ?? 'https://via.placeholder.com/600x200' }}" alt="">
         </div>
